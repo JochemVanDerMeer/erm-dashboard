8806c6d55168d8698382f0005832e8601a9c8c62
jochem-vandermeer <jochem.van.der.meer@topicus.nl>
2024-04-29 21:12:41 +0200

    update

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
 
-class Stage(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    current_stage = db.Column(db.String(20))
+class RowStage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stage_number = db.Column(db.String(50))
+    start = db.Column(db.String(50))
+    finish = db.Column(db.String(50))
+    distance = db.Column(db.String(50))
+    cox = db.Column(db.String(50))
+    bow = db.Column(db.String(50))
+    stroke = db.Column(db.String(50))
+    next_cox = db.Column(db.String(50))
+    next_bow = db.Column(db.String(50))
+    next_stroke = db.Column(db.String(50))
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,19 +1,60 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template
 
 from .extensions import db
 from .models import User
-from .models import Stage
+from .models import RowStage
 
 main = Blueprint('main', __name__)
 
+#@main.route('/')
+#def mainpage():
+#    stage = User.query.all()
+#    stage = stage[0]
+#   return render_template("./index.html", current_stage=stage)
+
+#@main.route('/current_stages')
+#def index():
+#    users = User.query.all()
+#    users_list_html = [f"<li>{ user.username }</li>" for user in users]
+#    return f"<ul>{''.join(users_list_html)}</ul>"
+
+#@main.route('/add/<username>')
+#def add_user(username):
+#    db.session.add(User(username=username))
+#    db.session.commit()
+#    return redirect(url_for("main.index"))
+
+#@main.route('/delete/<username>')
+#def delete_user(username):
+#    to_delete = User.query.filter_by(username = username).first()
+#    db.session.delete(to_delete)
+#    db.session.commit()
+#    return redirect(url_for("main.index"))
+
+
+
+#---------
 @main.route('/')
+def mainpage():
+    stage = RowStage.query.all()
+    stage = stage[0]
+    return render_template("./index.html", stage=stage )
+
+@main.route('/current_stages')
 def index():
-    users = Stage.query.all()
-    users_list_html = [f"<li>{ user.current_stage }</li>" for user in users]
+    users = RowStage.query.all()
+    users_list_html = [f"<li>{ user.stage_number }</li>" for user in users]
     return f"<ul>{''.join(users_list_html)}</ul>"
 
-@main.route('/add/<stage>')
-def add_stage(stage):
-    db.session.add(Stage(current_stage=stage))
+@main.route('/add/<stage_number>/<start>/<finish>/<distance>/<cox>/<bow>/<stroke>/<next_cox>/<next_bow>/<next_stroke>')
+def add_stage(stage_number, start, finish, distance, cox, bow, stroke, next_cox, next_bow, next_stroke):
+    db.session.add(RowStage(stage_number=stage_number, start=start, finish=finish, distance=distance, cox=cox, bow=bow, stroke=stroke, next_cox=next_cox, next_bow=next_bow, next_stroke=next_stroke))
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
+@main.route('/delete/<stage_number>')
+def delete_user(stage_number):
+    to_delete = RowStage.query.filter_by(stage_number = stage_number).first()
+    db.session.delete(to_delete)
     db.session.commit()
     return redirect(url_for("main.index"))
