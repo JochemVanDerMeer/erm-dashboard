da6552b0fa65029af3223046d9664fa044f2d31c
jochem-vandermeer <jochem.van.der.meer@topicus.nl>
2024-04-29 21:42:34 +0200

    minor update

--- myapp/routes.py
+++ myapp/routes.py
@@ -6,34 +6,6 @@ from .models import RowStage
 
 main = Blueprint('main', __name__)
 
-#@main.route('/')
-#def mainpage():
-#    stage = User.query.all()
-#    stage = stage[0]
-#   return render_template("./index.html", current_stage=stage)
-
-#@main.route('/current_stages')
-#def index():
-#    users = User.query.all()
-#    users_list_html = [f"<li>{ user.username }</li>" for user in users]
-#    return f"<ul>{''.join(users_list_html)}</ul>"
-
-#@main.route('/add/<username>')
-#def add_user(username):
-#    db.session.add(User(username=username))
-#    db.session.commit()
-#    return redirect(url_for("main.index"))
-
-#@main.route('/delete/<username>')
-#def delete_user(username):
-#    to_delete = User.query.filter_by(username = username).first()
-#    db.session.delete(to_delete)
-#    db.session.commit()
-#    return redirect(url_for("main.index"))
-
-
-
-#---------
 @main.route('/')
 def mainpage():
     stage = RowStage.query.all()
