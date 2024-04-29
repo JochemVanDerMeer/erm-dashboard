a53b83b26f74513a10e29e91a4598dd2ec61ea0c
jochem-vandermeer <jochem.van.der.meer@topicus.nl>
2024-04-29 20:15:17 +0200

    test

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,7 @@ from .extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
+
+class Stage(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    current_stage = db.Column(db.String(20))
--- myapp/routes.py
+++ myapp/routes.py
@@ -2,17 +2,18 @@ from flask import Blueprint, redirect, url_for
 
 from .extensions import db
 from .models import User
+from .models import Stage
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    users = User.query.all()
-    users_list_html = [f"<li>{ user.username }</li>" for user in users]
+    users = Stage.query.all()
+    users_list_html = [f"<li>{ user.current_stage }</li>" for user in users]
     return f"<ul>{''.join(users_list_html)}</ul>"
 
-@main.route('/add/<username>')
-def add_user(username):
-    db.session.add(User(username=username))
+@main.route('/add/<stage>')
+def add_stage(stage):
+    db.session.add(Stage(current_stage=stage))
     db.session.commit()
     return redirect(url_for("main.index"))
