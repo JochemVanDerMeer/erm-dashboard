333a2f74753aa6f62d7786a52065b2ac42a304c3
jochem-vandermeer <jochem.van.der.meer@topicus.nl>
2024-05-13 16:10:09 +0200

    final update

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -9,7 +9,6 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    #postgresql://erm_dashboard_user:0pR6KYRY1gFbfDAMj1wEKRI2o4eRG5BN@dpg-contl121hbls73fqsld0-a.frankfurt-postgres.render.com/erm_dashboard
 
     db.init_app(app)
 
